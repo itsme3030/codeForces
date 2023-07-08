e5b827219619ed92ba4be235f90be84082b9fe2d
Harshil <savaliyaharshil3030@gmail.com>
2023-07-08 15:47:00 +0530

    Add solution for problem `A - Nauuo and Votes` Link: https://codeforces.com/contest/1173/problem/A Tags: greedy, *800 Ref: https://codeforces.com/contest/1173/submission/212808296

---
+++ codeforces/1173/A.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+//defines
+#define ll  long long
+#define vll   vector<ll>
+#define vvll  vector<vector<ll>>
+#define pb    push_back
+#define popb  pop_back
+#define fi     first
+#define se     second
+#define nl  "\n"
+#define cy cout<<"YES"<<nl;
+#define cn cout<<"NO"<<nl;
+#define gcd(a, b) __gcd((a), (b))
+#define lcm(a, b) (a * b) / gcd(a, b)
+const ll mod = 1000000007;
+const long double pi=3.14159265358979323846264338327950288419716939937510582097494459230;
+
+
+//functions
+void csort(vector <ll>);
+void csort(vector <int>);
+void csort(vector <ll> &v){
+	ll i = 0;
+	while(i < v.size()){
+		ll cur = v[i] - 1;
+		if(cur < v.size() && v[i] != v[cur]){
+			swap(v[i],v[cur]);
+		}else  
+			i++;
+ 	}
+}
+void csort(vector <int> &v){
+	ll i = 0;
+	while(i < v.size()){
+		ll cur = v[i] - 1;
+		if(cur < v.size() && v[i] != v[cur]){
+			swap(v[i],v[cur]);
+		}else  
+			i++;
+ 	}
+}
+int main(){
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+ios_base::sync_with_stdio(0);
+cin.tie(0);cout.tie(0);
+	int x,y,z;
+	cin >> x >> y >> z;
+	if (x > y){
+		if (x > y + z) {
+			cout << '+' << endl;
+		} else {
+			cout << '?' << endl;
+		}
+	} else if (x < y) {
+		if (x + z < y) {
+			cout << '-' << endl;
+		} else {
+			cout << '?' << endl;
+		}
+	} else {
+		if (z == 0) {
+			cout << '0' << endl;
+		} else {
+			cout << '?' << endl;
+		} 
+	}
+	
+return 0;
+}
+
+ 	
