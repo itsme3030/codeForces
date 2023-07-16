8154855c117e61a3b5212b9fd3e61e0b5604bca8
Harshil <savaliyaharshil3030@gmail.com>
2023-07-16 10:51:00 +0530

    Add solution for problem `A - Sasha and His Trip` Link: https://codeforces.com/contest/1113/problem/A Tags: dp, greedy, math, *900 Ref: https://codeforces.com/contest/1113/submission/213985043

---
+++ codeforces/1113/A.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+//defines
+#define ll  long long
+#define vll   vector<ll>
+#define vvll  vector<vector<ll>>
+#define pb    push_back
+#define popb  pop_back
+#define fi     first
+#define se     second
+#define nl  "\n"
+#define cy cout<<"YES"<<nl;
+#define cn cout<<"NO"<<nl;
+#define gcd(a, b) __gcd((a), (b))
+#define lcm(a, b) (a * b) / gcd(a, b)
+const ll mod = 1000000007;
+const long double pi=3.14159265358979323846264338327950288419716939937510582097494459230;
+
+
+//functions
+void csort(vector <ll>);
+void csort(vector <int>);
+void csort(vector <ll> &v){
+	ll i = 0;
+	while(i < v.size()){
+		ll cur = v[i] - 1;
+		if(cur < v.size() && v[i] != v[cur]){
+			swap(v[i],v[cur]);
+		}else  
+			i++;
+ 	}
+}
+void csort(vector <int> &v){
+	ll i = 0;
+	while(i < v.size()){
+		ll cur = v[i] - 1;
+		if(cur < v.size() && v[i] != v[cur]){
+			swap(v[i],v[cur]);
+		}else  
+			i++;
+ 	}
+}
+int main(){
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+ios_base::sync_with_stdio(0);
+cin.tie(0);cout.tie(0);
+  //write down your solution from here
+		long n, v;
+		cin >> n >> v;
+		long sm = 0;
+		n--;
+		long tenk = 0; long cnt = 1;
+		while (n > 0){
+		if (n > tenk) {
+			if (v- tenk > 0){
+			if (v > n){
+				sm += (n - tenk) * cnt; cnt++;
+				tenk = n;
+			}else{
+			sm += (v - tenk) * cnt; cnt++; 
+			tenk = v;
+			} 
+		}
+		}tenk--; n--; 		
+	}
+	cout << sm << endl;
+	  
+return 0;
+}
+
+ 
