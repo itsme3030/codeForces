405816dcfae268eab3f1b095cedb349ec62fce05
Harshil <savaliyaharshil3030@gmail.com>
2023-08-27 20:19:00 +0530

    Add solution for problem `D - Gold Rush` Link: https://codeforces.com/contest/1829/problem/D Tags: brute force, dfs and similar, dp, implementation, *1000 Ref: https://codeforces.com/contest/1829/submission/220733924

---
+++ codeforces/1829/D.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+//defines
+#define ll  long long
+#define vll   vector<ll>
+#define vvll  vector<vector<ll>>
+#define pb    push_back
+#define popb  pop_back
+#define fi     first
+#define se     second
+#define nl  "\n"
+#define cy cout<<"YES"<<nl;
+#define cn cout<<"NO"<<nl;
+#define gcd(a, b) __gcd((a), (b))
+#define lcm(a, b) (a * b) / gcd(a, b)
+const ll mod = 1000000007;
+const long double pi=3.14159265358979323846264338327950288419716939937510582097494459230;
+
+
+//functions
+void solve();
+void csort(vector <ll>);
+void csort(vector <int>);
+void csort(vector <ll> &v){
+	ll i = 0;
+	while(i < v.size()){
+		ll cur = v[i] - 1;
+		if(cur < v.size() && v[i] != v[cur]){
+			swap(v[i],v[cur]);
+		}else  
+			i++;
+ 	}
+}
+void csort(vector <int> &v){
+	ll i = 0;
+	while(i < v.size()){
+		ll cur = v[i] - 1;
+		if(cur < v.size() && v[i] != v[cur]){
+			swap(v[i],v[cur]);
+		}else  
+			i++;
+ 	}
+}
+void makeset (ll n, unordered_set <ll> &s) {
+	s.insert(n);
+	if (n % 3) return;
+	s.insert (n / 3);
+	makeset(2 * (n / 3), s);
+	makeset(n / 3, s);
+}
+int main(){
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+ios_base::sync_with_stdio(0);
+cin.tie(0);cout.tie(0);
+
+	ll tt;
+	cin >> tt;
+        while(tt--)
+	solve();
+return 0;
+}
+
+void solve(){
+	//write down your solution from here	
+	long long n, m;
+	cin >> n >> m;
+	unordered_set <ll> s;
+	makeset (n, s);
+	if (s.find(m) != s.end()) { cy; }
+	else cn;
+
+	// for (auto x : s) cout << x <<" ";
+		// cout << endl << endl;
+}
+
+
+ 
