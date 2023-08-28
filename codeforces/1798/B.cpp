200dd032df96fc36ddd90e39673df2b105dbd9cb
Harshil <savaliyaharshil3030@gmail.com>
2023-08-28 20:02:00 +0530

    Add solution for problem `B - Three Sevens` Link: https://codeforces.com/contest/1798/problem/B Tags: brute force, data structures, greedy, implementation, *1000 Ref: https://codeforces.com/contest/1798/submission/220864613

---
+++ codeforces/1798/B.cpp
@@ -0,0 +1,115 @@
+#include<bits/stdc++.h>
+using namespace std;
+//defines
+#define ll  long long
+#define vll   vector<ll>
+#define vvll  vector<vector<ll>>
+#define pb    push_back
+#define popb  pop_back
+#define ff     first
+#define ss     second
+#define nl  "\n"
+#define cy cout<<"YES"<<nl;
+#define cn cout<<"NO"<<nl;
+#define gcd(a, b) __gcd((a), (b))
+#define lcm(a, b) (a * b) / gcd(a, b)
+const ll mod = 1000000007;
+const long double pi=3.14159265358979323846264338327950288419716939937510582097494459230;
+
+
+//functions
+void solve();
+void csort(vector <ll>);
+void csort(vector <int>);
+void csort(vector <ll> &v){
+	ll i = 0;
+	while(i < v.size()){
+		ll cur = v[i] - 1;
+		if(cur < v.size() && v[i] != v[cur]){
+			swap(v[i],v[cur]);
+		}else  
+			i++;
+ 	}
+}
+void csort(vector <int> &v){
+	ll i = 0;
+	while(i < v.size()){
+		ll cur = v[i] - 1;
+		if(cur < v.size() && v[i] != v[cur]){
+			swap(v[i],v[cur]);
+		}else  
+			i++;
+ 	}
+}
+vector<int> sieve_of_eratosthenes(int limit) {
+    std::vector<bool> primes(limit + 1, true);
+    primes[0] = primes[1] = false;  // 0 and 1 are not prime
+
+    for (int num = 2; num * num <= limit; ++num) {
+        if (primes[num]) {
+            for (int multiple = num * num; multiple <= limit; multiple += num) {
+                primes[multiple] = false;
+            }
+        }
+    }
+
+    std::vector<int> prime_numbers;
+    for (int num = 2; num <= limit; ++num) {
+        if (primes[num]) {
+            prime_numbers.push_back(num);
+        }
+    }
+
+    return prime_numbers;
+}
+int main(){
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+ios_base::sync_with_stdio(0);
+cin.tie(0);cout.tie(0);
+
+	ll tt;
+	cin >> tt;
+        while(tt--)
+	solve();
+return 0;
+}
+
+void solve(){
+	//write down your solution from here	
+	ll n;
+	cin >> n;
+	vector<vector<ll>> v(n);
+	unordered_map <ll, ll> mp;
+	vector <ll> ans;
+	for (int i = 0; i < n; i++) {
+		int m; cin >> m;
+		v[i].resize(m);
+		for (auto &x : v[i]) {
+			cin >> x; mp[x]++; }
+	}
+	int flg = 0;
+	int found = 0;
+	vector <ll> erase;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < v[i].size(); j++) {
+			mp[v[i][j]]--;
+			if (mp[v[i][j]] == 0 && flg == 0) {
+				ans.push_back(v[i][j]); flg = 1; found = 1;
+			} 
+			 if (mp[v[i][j]] == 0) mp.erase(v[i][j]); 
+		} flg = 0;
+		if (found == 0) {
+			cout << -1 << endl;
+			return;
+		}found = 0;
+	}
+
+	for (auto x : ans) cout << x << " ";
+	cout << endl;
+}
+
+ 
