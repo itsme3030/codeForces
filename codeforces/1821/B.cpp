c8fc3b10842cc1b775284efe8f7af8ecd1e7abd0
Harshil <savaliyaharshil3030@gmail.com>
2023-08-31 18:36:00 +0530

    Add solution for problem `B - Sort the Subarray` Link: https://codeforces.com/contest/1821/problem/B Tags: brute force, greedy, *1100 Ref: https://codeforces.com/contest/1821/submission/221266067

---
+++ codeforces/1821/B.cpp
@@ -0,0 +1,112 @@
+#include<bits/stdc++.h>
+using namespace std;
+//defines
+#define ll    long long
+#define vll   vector<ll>
+#define vvll  vector<vector<ll>>
+#define pb    push_back
+#define popb  pop_back
+#define ff    first
+#define ss    second
+#define pll   pair<ll,ll>
+#define nl    "\n"
+#define cy    cout<<"YES"<<nl;
+#define cn    cout<<"NO"<<nl;
+#define gcd(a, b) __gcd((a), (b))
+#define lcm(a, b) (a * b) / gcd(a, b)
+const ll mod = 1000000007;
+const long double pi=3.14159265358979323846264338327950288419716939937510582097494459230;
+
+
+//functions
+void solve();
+void csort(vector <ll>);
+void csort(vector <int>);
+void csort(vector <ll> &v){
+	ll i = 0;
+	while(i < v.size()){
+		ll cur = v[i] - 1;
+		if(cur < v.size() && v[i] != v[cur]){
+			swap(v[i],v[cur]);
+		}else  
+			i++;
+ 	}
+}
+void csort(vector <int> &v){
+	ll i = 0;
+	while(i < v.size()){
+		ll cur = v[i] - 1;
+		if(cur < v.size() && v[i] != v[cur]){
+			swap(v[i],v[cur]);
+		}else  
+			i++;
+ 	}
+}
+vector<int> sieve_of_eratosthenes(int limit) {
+    std::vector<bool> primes(limit + 1, true);
+    primes[0] = primes[1] = false;  // 0 and 1 are not prime
+
+    for (int num = 2; num * num <= limit; ++num) {
+        if (primes[num]) {
+            for (int multiple = num * num; multiple <= limit; multiple += num) {
+                primes[multiple] = false;
+            }
+        }
+    }
+
+    std::vector<int> prime_numbers;
+    for (int num = 2; num <= limit; ++num) {
+        if (primes[num]) {
+            prime_numbers.push_back(num);
+        }
+    }
+
+    return prime_numbers;
+}
+int main(){
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+ios_base::sync_with_stdio(0);
+cin.tie(0);cout.tie(0);
+
+	ll tt; cin >> tt;
+	while(tt--)
+	solve();
+return 0;
+}
+
+void solve(){
+	//write down your solution from here	
+	 ll n;
+	 cin >> n;
+	 vector<int> a(n);
+	 vector<int> a2(n);
+	 for (auto &x : a) cin >> x;
+	 for (auto &x : a2) cin >> x;
+
+	 	int l = 0;
+		while (l < n) {
+		if (a[l] != a2[l]) break;
+		l++;
+		}
+		int r = n - 1;
+		while (r >= 0) {
+			if (a[r] != a2[r]) break;
+			r--;
+		}
+		sort(a.begin() + l, a.begin() + r + 1);
+		while (l > 0) {
+			if (a[l] >= a[l - 1]) l--;
+			else break;
+		}
+		while (r < n - 1){
+			if (a[r] <= a[r + 1]) r++;
+			else break;
+		}
+		cout << l + 1 << " " << r + 1 << "\n";
+}
+
+ 
