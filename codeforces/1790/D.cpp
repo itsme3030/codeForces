a0155819f64373d8c237773058202223c0548390
Harshil <savaliyaharshil3030@gmail.com>
2023-09-14 21:24:00 +0530

    Add solution for problem `D - Matryoshkas` Link: https://codeforces.com/contest/1790/problem/D Tags: data structures, greedy, sortings, *1200 Ref: https://codeforces.com/contest/1790/submission/223377491

--- codeforces/1790/D.cpp
+++ codeforces/1790/D.cpp
@@ -1,6 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-void solve();
+void solve() {
+	int n;
+	cin >> n;
+	map<int, int> mp;
+	set<int> st;
+	vector <int> v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+		mp[v[i]]++;
+		st.insert(v[i]);
+		st.insert(v[i] + 1);
+	}
+	int last = 0;
+	int cnt = 0;	
+	for (auto x : st) {
+		int freq = mp[x];
+		cnt += max(0, freq - last);
+		last = freq;
+	}
+	cout << cnt << "\n";
+};
 int main(){
 
 // ----------------- IO & IO Display Settings -----------------------//
@@ -12,39 +32,8 @@ ios_base::sync_with_stdio(0);
 cin.tie(0);cout.tie(0);
 // ----------------- IO & IO Display Settings -----------------------//
 
-long long tt; cin >> tt;
+int tt; cin >> tt;
 while(tt--) solve();
 return 0;
 }
 
-void solve(){
-	//write down for each test case
-	long long n;
-	cin >> n;
-	vector <long long> v(n);
-	for (auto &x : v) cin >> x;
-	map<long long, long long> mp;
-	long long mx = 1;
-
-	for (auto &x : v) mp[x]++;
-	vector<pair<long long, long long>> vp(n);
-	for (int i = 0; i < n; i++) {
-		vp[i].first = v[i];
-		vp[i].second = mp[v[i]];
-	}
-	auto comp = [&](pair<long long,long long> &p1, pair <long long, long long> &p2) {
-		return p1.first < p2.first;
-	};
-	sort(vp.begin(), vp.end(), comp); mp.clear();
-	mp[v[0]]++;
-	long long cnt = 0;
-	cnt = vp[0].second;
-	for (int i = 1; i < n; i++) {
-		if (vp[i - 1].first + 1 < vp[i].first) {
-			cnt += vp[i].second;
-		}else if (vp[i].second > vp[i - 1].second) {
-			cnt += vp[i].second - vp[i - 1].second;
-		}
-	}
-	cout << cnt << "\n";
-}
