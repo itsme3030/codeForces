44a2345a8863687473f3dac728206a89994c6d1e
Harshil <savaliyaharshil3030@gmail.com>
2023-09-29 12:22:00 +0530

    Add solution for problem `D - 1D Eraser` Link: https://codeforces.com/contest/1873/problem/D Tags: greedy, implementation, two pointers, *800 Ref: https://codeforces.com/contest/1873/submission/225743155

--- codeforces/1873/D.java
+++ codeforces/1873/D.java
@@ -3,152 +3,12 @@ import java.util.*;
 
 public class quester_013 {
 	public static void main(String[] args) {
-		try {
-            InputReader in = new InputReader(System.in);
-            PrintWriter out = new PrintWriter(System.out);
-            int testCases=in.nextInt();
-            while(testCases-- > 0) {
-            Solver.solve(in, out);
-            }
-            out.close();
-        } catch (Exception e) {
-            return;
-        }
-	}
-}
-
-class Triple<A, B, C> {
-    public A a;
-    public B b;
-    public C c;
-
-    public Triple(A first, B second, C third) {
-        a = first;
-        b = second;
-        c = third;
-    }
-}
-class Pair<A, B> {
-    public A a;
-    public B b;
-
-    public Pair(A a, B b) {
-        this.a = a;
-        this.b = b;
-    }
-}
-
-class InputReader {
- 
-		private final InputStream stream;
-		private final byte[] buf = new byte[8192];
-		private int curChar, snumChars;
- 
-		public InputReader(InputStream st) {
-			this.stream = st;
-		}
- 
-		public int read() {
-			if (snumChars == -1)
-				throw new InputMismatchException();
-			if (curChar >= snumChars) {
-				curChar = 0;
-				try {
-					snumChars = stream.read(buf);
-				} catch (IOException e) {
-					throw new InputMismatchException();
-				}
-				if (snumChars <= 0)
-					return -1;
-			}
-			return buf[curChar++];
-		}
- 
-		public int nextInt() {
-			int c = read();
-			while (isSpaceChar(c)) {
-				c = read();
-			}
-			int sgn = 1;
-			if (c == '-') {
-				sgn = -1;
-				c = read();
-			}
-			int res = 0;
-			do {
-				res *= 10;
-				res += c - '0';
-				c = read();
-			} while (!isSpaceChar(c));
-			return res * sgn;
-		}
- 
-		public long nextLong() {
-			int c = read();
-			while (isSpaceChar(c)) {
-				c = read();
-			}
-			int sgn = 1;
-			if (c == '-') {
-				sgn = -1;
-				c = read();
-			}
-			long res = 0;
-			do {
-				res *= 10;
-				res += c - '0';
-				c = read();
-			} while (!isSpaceChar(c));
-			return res * sgn;
-		}
- 
-		public int[] nextIntArray(int n) {
-			int a[] = new int[n];
-			for (int i = 0; i < n; i++) {
-				a[i] = nextInt();
-			}
-			return a;
-		}
- 
-		public String readString() {
-			int c = read();
-			while (isSpaceChar(c)) {
-				c = read();
-			}
-			StringBuilder res = new StringBuilder();
-			do {
-				res.appendCodePoint(c);
-				c = read();
-			} while (!isSpaceChar(c));
-			return res.toString();
-		}
- 
-		public String nextLine() {
-			int c = read();
-			while (isSpaceChar(c))
-				c = read();
-			StringBuilder res = new StringBuilder();
-			do {
-				res.appendCodePoint(c);
-				c = read();
-			} while (!isEndOfLine(c));
-			return res.toString();
-		}
- 
-		public boolean isSpaceChar(int c) {
-			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
-		}
- 
-		private boolean isEndOfLine(int c) {
-			return c == '\n' || c == '\r' || c == -1;
-		}
-	}
-
-class Solver {
-	 static void solve (InputReader in, PrintWriter out) throws IOException {
-		int n = in.nextInt();
-		int k = in.nextInt();
-		String s = in.readString();
+		Scanner sc = new Scanner (System.in);
+		int tt = sc.nextInt();
+		while (tt-- > 0) {
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		String s = sc.next();
 		ArrayList <Integer> li = new ArrayList<>();
 		int cnt = 0;
 		for (int i = 0; i < s.length(); i++) {
@@ -156,7 +16,8 @@ class Solver {
 			else cnt++;
 		}
 		if (cnt == s.length()) {
-			out.println(0); return;
+			System.out.println(0);
+			continue;
 		}
 		 cnt = 1;
 		int temp = li.get(li.size() - 1);
@@ -165,7 +26,7 @@ class Solver {
 				cnt++; temp = li.get(i);
 			}
 		}
-		out.println(cnt);
-	 }
+		System.out.println(cnt);
+		}
+	}
 }
- 
