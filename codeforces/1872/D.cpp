3d57d9060d5c8ef6f4ceb16eb1535fab1a741806
Harshil <savaliyaharshil3030@gmail.com>
2023-10-02 14:38:00 +0530

    Add solution for problem `D - Plus Minus Permutation` Link: https://codeforces.com/contest/1872/problem/D Tags: math, *1200 Ref: https://codeforces.com/contest/1872/submission/226268719

---
+++ codeforces/1872/D.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+//defines
+#define ll    long long
+#define pb    push_back
+#define popb  pop_back
+#define ff    first
+#define ss    second
+#define pll   pair<ll,ll>
+#define nl    "\n"
+#define gcd(a, b) ((b) == 0 ? (a) : gcd((b), (a) % (b)))
+#define lcm(a, b) (a * b) / gcd(a, b)
+const ll mod = 1000000007;
+const long double pi=3.14159265358979323846264338327950288419716939937510582097494459230;
+
+//functions
+void solve();
+void csort(vector <ll>);
+void csort(vector <int>);
+void csort(vector <ll> &v){
+    ll i = 0;
+    while(i < v.size()){
+        ll cur = v[i] - 1;
+        if(cur < v.size() && v[i] != v[cur]){
+            swap(v[i],v[cur]);
+        }else
+            i++;
+    }
+}
+void csort(vector <int> &v){
+    ll i = 0;
+    while(i < v.size()){
+        ll cur = v[i] - 1;
+        if(cur < v.size() && v[i] != v[cur]){
+            swap(v[i],v[cur]);
+        }else
+            i++;
+    }
+}
+vector<int> sieve_of_eratosthenes(int limit) {
+    std::vector<bool> primes(limit + 1, true);
+    primes[0] = primes[1] = false;  // 0 and 1 are not prime
+
+    for (int num = 2; num * num <= limit; ++num) {
+        if (primes[num]) {
+            for (int multiple = num * num; multiple <= limit; multiple += num) {
+                primes[multiple] = false;
+            }
+        }
+    }
+
+    std::vector<int> prime_numbers;
+    for (int num = 2; num <= limit; ++num) {
+        if (primes[num]) {
+            prime_numbers.push_back(num);
+        }
+    }
+
+    return prime_numbers;
+}
+int main(){
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+
+    ll tt; cin >> tt;
+    while(tt--)
+        solve();
+    return 0;
+}
+
+void solve(){
+    //write down your solution from here
+    ll n, x, y; cin >> n >> x >> y;
+    ll first = n/x;
+    ll second = n/y;
+    ll get = lcm(x, y);
+    ll both = (x % y == 0 || y % x == 0) ? n/max(x,y) : n/get;
+//    cout << "both" << both << " " << n/lcm(x,y) << "\n";
+    first -= both;
+    second -= both;
+    ll sm = (n * (n + 1))/2 - ((n - first) * (n - first + 1)) / 2;
+    sm -= (second * (second + 1))/2;
+    cout << sm << "\n";
+}
+
