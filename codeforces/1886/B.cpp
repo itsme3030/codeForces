a9f29d07ab6e02f6da627b6c0fa9f73235ee76f6
Harshil <savaliyaharshil3030@gmail.com>
2023-10-14 20:50:00 +0530

    Add solution for problem `B - Fear of the Dark` Link: https://codeforces.com/contest/1886/problem/B Tags: binary search, geometry, math, *1200 Ref: https://codeforces.com/contest/1886/submission/228188950

---
+++ codeforces/1886/B.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+using namespace std;
+//defines
+#define ll    long long
+#define pb    push_back
+#define popb  pop_back
+#define ff    first
+#define ss    second
+#define pll   pair<ll,ll>
+#define nl    "\n"
+#define gcd(a, b) ((b) == 0 ? (a) : gcd((b), (a) % (b)))
+#define lcm(a, b) (a * b) / gcd(a, b)
+const ll mod = 1000000007;
+const long double pi=3.14159265358979323846264338327950288419716939937510582097494459230;
+
+//functions
+void solve();
+ll bin_expo(ll a, ll b, ll MOD = mod) {
+    ll ans = 1;
+    while (b) {
+        if (b % 2) ans = ans * a % MOD;
+        a = a * a % MOD;
+        b /= 2;
+    } return ans;
+}
+
+ll inverse(ll a) {
+    return bin_expo(a, mod - 2, mod);
+}
+void csort(vector <ll>);
+void csort(vector <int>);
+void csort(vector <ll> &v){
+    ll i = 0;
+    while(i < v.size()){
+        ll cur = v[i] - 1;
+        if(cur < v.size() && v[i] != v[cur]){
+            swap(v[i],v[cur]);
+        }else
+            i++;
+    }
+}
+void csort(vector <int> &v){
+    ll i = 0;
+    while(i < v.size()){
+        ll cur = v[i] - 1;
+        if(cur < v.size() && v[i] != v[cur]){
+            swap(v[i],v[cur]);
+        }else
+            i++;
+    }
+}
+vector<int> sieve_of_eratosthenes(int limit) {
+    std::vector<bool> primes(limit + 1, true);
+    primes[0] = primes[1] = false;  // 0 and 1 are not prime
+    for (int num = 2; num * num <= limit; ++num) {
+        if (primes[num]) {
+            for (int multiple = num * num; multiple <= limit; multiple += num) {
+                primes[multiple] = false;
+            }
+        }
+    }
+    std::vector<int> prime_numbers;
+    for (int num = 2; num <= limit; ++num) {
+        if (primes[num]) {
+            prime_numbers.push_back(num);
+        }
+    }
+    return prime_numbers;
+}
+int main(){
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    ll tt; cin >> tt;
+    while(tt--)
+        solve();
+    return 0;
+}
+
+void solve(){
+    double px , py; cin >> px >> py;
+    double ax , ay; cin >> ax >> ay;
+    double bx, by; cin >> bx >> by;
+
+    double dis1 = sqrt((abs(ax - bx) * abs(ax - bx)) + (abs(ay - by) * abs(ay - by)));
+    double disWithA = sqrt(abs((px - ax) * (px - ax)) + (abs((py - ay) * (py - ay))));
+    double disWithB = sqrt(abs((px - bx) * (px - bx)) + (abs((py - by) * (py - by))));
+    double disWithAandO = sqrt(abs(ax * ax) + abs(ay * ay));
+    double disWithBandO = sqrt(abs(bx * bx) + abs(by * by));
+    double  w = dis1/2.0;
+    if ((w >= disWithA || w >= disWithB) && (w >= disWithAandO || w >= disWithBandO)) {
+        w = min(w, min(max(disWithA, disWithAandO), max(disWithB, disWithBandO)));
+        printf("%.7f\n",w);
+        return;
+    }
+    w = max(min(disWithA, disWithB), min(disWithBandO, disWithAandO));
+    printf("%.10f\n", w);
+}
