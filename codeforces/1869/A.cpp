23507abf3e801bb138d6adcca89012382c3fb800
Harshil <savaliyaharshil3030@gmail.com>
2023-10-17 14:53:00 +0530

    Add solution for problem `A - Make It Zero` Link: https://codeforces.com/contest/1869/problem/A Tags: constructive algorithms, *900 Ref: https://codeforces.com/contest/1869/submission/228519473

--- codeforces/1869/A.cpp
+++ codeforces/1869/A.cpp
@@ -1,22 +1,98 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int main() {
-    int tt; cin >> tt;
-    while (tt--){
-        int n; cin >> n;
-        vector <int> v(n);
-        for (auto &x : v) cin >> x;
-        if (n % 2 == 0) {
-            cout << 2 << "\n";
-            cout << 1 << " " << n << "\n";
-            cout << 1 << " " << n << "\n";
-        }else {
-            cout << 4 << "\n";
-            cout << 2 << " " << n << "\n";
-            cout << 2 << " " << n << "\n";
-            cout << 1 << " " << 2 << "\n";
-            cout << 1 << " " << 2 << "\n";
+//defines
+#define ll    long long
+#define pb    push_back
+#define popb  pop_back
+#define ff    first
+#define ss    second
+#define pll   pair<ll,ll>
+#define nl    "\n"
+#define gcd(a, b) ((b) == 0 ? (a) : gcd((b), (a) % (b)))
+#define lcm(a, b) (a * b) / gcd(a, b)
+const ll mod = 1000000007;
+const long double pi=3.14159265358979323846264338327950288419716939937510582097494459230;
+
+//functions
+void solve();
+ll bin_expo(ll a, ll b, ll MOD = mod) {
+    ll ans = 1;
+    while (b) {
+        if (b % 2) ans = ans * a % MOD;
+        a = a * a % MOD;
+        b /= 2;
+    } return ans;
+}
+
+ll inverse(ll a) {
+    return bin_expo(a, mod - 2, mod);
+}
+void csort(vector <ll>);
+void csort(vector <int>);
+void csort(vector <ll> &v){
+    ll i = 0;
+    while(i < v.size()){
+        ll cur = v[i] - 1;
+        if(cur < v.size() && v[i] != v[cur]){
+            swap(v[i],v[cur]);
+        }else
+            i++;
+    }
+}
+void csort(vector <int> &v){
+    ll i = 0;
+    while(i < v.size()){
+        ll cur = v[i] - 1;
+        if(cur < v.size() && v[i] != v[cur]){
+            swap(v[i],v[cur]);
+        }else
+            i++;
+    }
+}
+vector<int> sieve_of_eratosthenes(int limit) {
+    std::vector<bool> primes(limit + 1, true);
+    primes[0] = primes[1] = false;  // 0 and 1 are not prime
+    for (int num = 2; num * num <= limit; ++num) {
+        if (primes[num]) {
+            for (int multiple = num * num; multiple <= limit; multiple += num) {
+                primes[multiple] = false;
+            }
         }
     }
+    std::vector<int> prime_numbers;
+    for (int num = 2; num <= limit; ++num) {
+        if (primes[num]) {
+            prime_numbers.push_back(num);
+        }
+    }
+    return prime_numbers;
+}
+ll tc = 0;
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    ll tt; cin >> tt;
+    while(tt--)
+        solve();
     return 0;
+}
+void solve(){
+    ll n; cin >> n;
+    vector <int> v(n);
+     for (auto &x : v) cin >> x;
+     if (n % 2 == 0) {
+         cout << 2 << "\n";
+         cout << 1 << " " << n << "\n";
+         cout << 1 << " " << n << "\n";
+     }else {
+         cout  << 4 << "\n";
+         cout << 2 << " " << n <<  "\n";
+         cout << 2 << " " << n << "\n";
+         cout << 1 << " " << 2 << "\n";
+         cout << 1 << " " << 2 << "\n";
+     }
 }
