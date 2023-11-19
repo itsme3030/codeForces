0d12303f75e5e6d1587d5527ad7d98e710eebac9
Harshil <savaliyaharshil3030@gmail.com>
2023-11-20 00:10:00 +0530

    Add solution for problem `B - Sets and Union` Link: https://codeforces.com/contest/1882/problem/B Tags: bitmasks, brute force, constructive algorithms, greedy, *1300 Ref: https://codeforces.com/contest/1882/submission/233487649

--- codeforces/1882/B.cpp
+++ codeforces/1882/B.cpp
@@ -10,37 +10,37 @@ const ll mod = 1000000007;
 const long double pi = 3.14159265358979323846264338327950288419716939937510582097494459230;
 void solve();
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    ll tt; cin >> tt;  while (tt--)
-        solve();
-    return 0;
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	ll tt; cin >> tt;  while (tt--)
+		solve();
+	return 0;
 }
 void solve() {
-    int n; cin >> n;
-    set<int> s;
-    set<int> st[n];
-    for (int i = 0; i < n; i++) {
-        int x; cin >> x;
-        for (int j = 0; j < x; j++) {
-            int y; cin >> y;
-            s.insert(y);
-            st[i].insert(y);
-        }
-    }
-    long long mx = 0;
-    long long ans = 0;
-    for (auto &x : s) {
-        set<int> temp;
-        for (int i = 0; i < n; i++) {
-            if (st[i].find(x) != st[i].end()) {
-                continue;
-            }else {
-                for (auto &y : st[i])
-                temp.insert(y);
-            }
-        }
-        mx = max(mx, (long long)temp.size());
-    }
-    cout << mx << "\n";
+	int n; cin >> n;
+	set<int> s;
+	set<int> st[n];
+	for (int i = 0; i < n; i++) {
+		int x; cin >> x;
+		for (int j = 0; j < x; j++) {
+			int y; cin >> y;
+			s.insert(y);
+			st[i].insert(y);
+		}
+	}
+	long long mx = 0;
+	long long ans = 0;
+	for (auto &x : s) {
+		set<int> temp;
+		for (int i = 0; i < n; i++) {
+			if (st[i].find(x) != st[i].end()) {
+				continue;
+			}else {
+				for (auto &y : st[i])
+				temp.insert(y);
+			}
+		}
+		mx = max(mx, (long long)temp.size());
+	}
+	cout << mx << "\n";
 }
