dd2fae5353d5c879c38f3733b507197dee84a6d0
Harshil <savaliyaharshil3030@gmail.com>
2023-11-19 14:52:00 +0530

    Add solution for problem `E1 - Rudolf and Snowflakes (simple version)` Link: https://codeforces.com/contest/1846/problem/E1 Tags: brute force, implementation, math, *1300 Ref: https://codeforces.com/contest/1846/submission/233401342

--- codeforces/1846/E1.cpp
+++ codeforces/1846/E1.cpp
@@ -10,45 +10,45 @@ const ll mod = 1000000007;
 const long double pi = 3.14159265358979323846264338327950288419716939937510582097494459230;
 void solve();
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    ll tt; cin >> tt;  while (tt--)
-        solve();
-    return 0;
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	ll tt; cin >> tt;  while (tt--)
+		solve();
+	return 0;
 }
 void solve() {
-    long long n; cin >> n;
-    auto bin_expo = [&] (long long a, int b) {
-        long long ans = 1;
-        while (b > 0) {
-            if (b % 2) {
-                ans = ans * a;
-            } b /= 2;
-            a = a * a;
-        }
-        return ans;
-    };
-    for (long long k = 2; k <= 100000; k++) {
-        long long mn = 1 + k + (k * k);
-        if (n < mn) {
-            cout << "NO\n";
-            return;
-        } else {
-            if (n == mn) {  
-                cout << "YES\n";
-                return;
-            } else {
-                long long diff = n - mn;
-                for (int i = 3; i <= 999; i++) {
-                    diff -= bin_expo(k, i);
-                    if (diff == 0) {
-                        cout << "YES\n";
-                        return;
-                    }else if (diff < 0) {
-                        break;
-                    }
-                }
-            }
-        }
-    }
+	long long n; cin >> n;
+	auto bin_expo = [&] (long long a, int b) {
+		long long ans = 1;
+		while (b > 0) {
+			if (b % 2) {
+				ans = ans * a;
+			} b /= 2;
+			a = a * a;
+		}
+		return ans;
+	};
+	for (long long k = 2; k <= 100000; k++) {
+		long long mn = 1 + k + (k * k);
+		if (n < mn) {
+			cout << "NO\n";
+			return;
+		} else {
+			if (n == mn) {
+				cout << "YES\n";
+				return;
+			} else {
+				long long diff = n - mn;
+				for (int i = 3; i <= 1000; i++) {
+					diff -= bin_expo(k, i);
+					if (diff == 0) {
+						cout << "YES\n";
+						return;
+					}else if (diff < 0) {
+						break;
+					}
+				}
+			}
+		}
+	}
 }
