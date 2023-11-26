600d6f30e079f5b3ceb055678c3e54df2edb34e1
Harshil <savaliyaharshil3030@gmail.com>
2023-11-26 16:52:00 +0530

    Add solution for problem `D - Flipper` Link: https://codeforces.com/contest/1833/problem/D Tags: brute force, constructive algorithms, greedy, *1400 Ref: https://codeforces.com/contest/1833/submission/234405875

--- codeforces/1833/D.cpp
+++ codeforces/1833/D.cpp
@@ -6,7 +6,6 @@ long long t = 0;
 #define lcm(a, b) (a * b) / gcd(a, b)
 const ll mod = 1000000007;
 const long double pi = 3.14159265358979323846264338327950288419716939937510582097494459230;
-#define int ll
 void solve();
 int bin_expo(int a, int b) {
     int ans = 1;
@@ -17,10 +16,10 @@ int bin_expo(int a, int b) {
         a = a * a;
     } return ans;
 }
-signed main() {
+int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    int tt; cin >> tt;  while (tt--)
+    ll tt; cin >> tt;  while (tt--)
         solve();
     return 0;
 }
@@ -28,77 +27,98 @@ signed main() {
 
 //sol.
 void solve() {
-    int n; cin >> n;
-    vector<int> v(n);
-    for (auto &x : v) cin >> x;
-
+    int n; cin >> n; vector<int> v(n); for (auto &x: v) cin >> x;
+    // vector<int> temp = v;
     vector<pair<int, int>> vp(n);
     for (int i = 0; i < n; i++) {
         vp[i].first = v[i];
         vp[i].second = i;
     }
-
     sort(vp.begin(), vp.end());
     int in1 = vp[n-1].second;
     int ele1 = vp[n-1].first;
-    if(n == 1) {
-        cout << v[0] << " "; cout << "\n";
+    if (n == 1) {
+        cout << v[0] << " ";
+        cout << "\n";
         return;
     }
     int in2 = vp[n-2].second;
     int ele2 = vp[n-2].first;
-    int left, right;
-
+    if (n == 2) {
+        cout << v[1] << " " << v[0] << "\n";
+        return; 
+    }
+    int in3 = vp[n-3].second;
+    int ele3 = vp[n-3].first;
+    int left = -1, right = -1;
     if (in1 == 0) {
-        if (in2 == n-1) {
-            int extraind = in2;
-            left = extraind, right = extraind;
-        } else {
-            int extraind = in2 - 1;
-            right = extraind;
-            left = extraind;
-            int i = 0; extraind--;
-            while (i < extraind) {
-                if (v[i] > v[extraind]) {
+            if (in2 == n-1) {
+                int exind = in2;
+                right = exind;
+                left = exind;
+                exind--;
+                int i = 0;
+                while(i < exind) {
+                if (v[i] > v[exind]) {
                     break;
                 } else {
                     left--;
-                    extraind--;
+                    exind--;
+                }
+            }    
+        } else {
+            int exind = in2-1;
+            int i = in1;
+            right = exind;
+            left = exind;
+            exind--;
+            while(i < exind) {
+                if (v[i] > v[exind]) {
+                    break;
+                } else {
+                    left--;
+                    exind--;
                 }
             }
         }
-    } else {
-        if (in1 == n-1) {
-            int extraind = in1;
-            right = extraind;
-            left = extraind;
-            int i = 0; extraind--;
-            while (i < extraind) {
-                if (v[i] > v[extraind]) {
-                    break;
-                } else {
-                    left--;
-                    extraind--;
-                }
+    } else if (in1 == n-1) {
+        int exind = in1;
+        right = exind;
+        left = exind;
+        exind--;
+        int i = 0;
+        while(i < exind) {
+            if (v[i] > v[exind]) {
+                break;
+            } else {
+                left--;
+                exind--;
             }
-        } else {
-            int extraind = in1 - 1;
-            right = extraind;
-            left = extraind;
-            int i = 0; extraind--;
-            while (i < extraind) {
-                if (v[i] > v[extraind]) {
-                    break;
-                } else {
-                    left--;
-                    extraind--;
-                }
+        }
+    }else {
+        int exind = in1 - 1;
+        int i = 0;
+        right = exind;
+        left = exind;
+        exind--;
+        while (i < exind) {
+            if (v[i] > v[exind]) {
+                break;
+            } else {
+                left--;
+                exind--;
             }
         }
     }
+    // cout << "left : " << left << " " << "right : " << right << "\n";
+    for (int i = right + 1; i < n; i++) {
+        cout << v[i] << " ";
+    } 
+    for (int i = right; i >= left; i--) {
+        cout << v[i] << " ";
+    }
+    for (int i = 0; i < left; i++) {
+        cout << v[i] << " ";
+    } cout << "\n";
 
-    for (int i = right + 1; i < n; i++) cout << v[i] << " ";
-    for (int i = right; i >= left; i--) cout << v[i] << " ";
-    for (int i = 0; i < left; i++) cout << v[i] << " ";
-    cout << "\n";
-}   
+}
