fdd037ed4d3b2e396b3fabc82930cfb2a145e0fb
Harshil <savaliyaharshil3030@gmail.com>
2023-11-26 16:08:00 +0530

    Add solution for problem `C - Vlad Building Beautiful Array` Link: https://codeforces.com/contest/1833/problem/C Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1833/submission/234400995

---
+++ codeforces/1833/C.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long t = 0;
+#define ll    long long int
+#define gcd(a, b) __gcd((a), (b))
+#define lcm(a, b) (a * b) / gcd(a, b)
+const ll mod = 1000000007;
+const long double pi = 3.14159265358979323846264338327950288419716939937510582097494459230;
+void solve();
+int bin_expo(int a, int b) {
+    int ans = 1;
+    while (b > 0) {
+        if (b % 2) {
+            ans = ans * a;
+        } b/=2;
+        a = a * a;
+    } return ans;
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    ll tt; cin >> tt;  while (tt--)
+        solve();
+    return 0;
+}
+//-----------------------------------------------------------------------------------------------------------------------
+
+//sol.
+void solve() {
+    int n; cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) cin >> v[i];
+    sort(v.begin(), v.end());
+    int evenflg = 0, oddflg = 0;
+    if (v[0] % 2) oddflg = 1;
+    else evenflg = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (evenflg && oddflg) {
+            break;
+        } else if(evenflg) {
+            if (v[i] % 2) {
+                cout << "NO\n";
+                return;
+            } 
+        } 
+    }
+    cout << "YES\n";
+}
