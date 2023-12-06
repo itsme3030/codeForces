0600857a19e4fd4c7328191375142fdd126858ac
Harshil <savaliyaharshil3030@gmail.com>
2023-12-06 22:41:00 +0530

    Add solution for problem `B - I Hate 1111` Link: https://codeforces.com/contest/1526/problem/B Tags: dp, math, number theory, *1400 Ref: https://codeforces.com/contest/1526/submission/236087001

---
+++ codeforces/1526/B.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long t = 0;
+#define ll    long long int
+#define gcd(a, b) __gcd((a), (b))
+#define lcm(a, b) (a * b) / gcd(a, b)
+const ll mod = 1000000007;
+const long double pi = 3.14159265358979323846264338327950288419716939937510582097494459230;
+#define int ll
+void solve();
+int bin_expo(int a, int b) {
+    int ans = 1;
+    while (b > 0) {
+        if (b % 2) {
+            ans = ans * a;
+        } b/=2;
+        a = a * a;
+    } return ans;
+}
+signed main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tt; cin >> tt;  while (tt--)
+        solve();
+    return 0;
+}
+//-----------------------------------------------------------------------------------------------------------------------
+
+void solve() {
+    int n; cin >> n;
+    if (n < 11) {
+        cout << "NO\n";
+        return;
+    }
+    int x = 0;
+    int mod = n % 11;
+    while (mod--) {
+        x += 111;
+    }
+
+    n -= x;
+    if (n % 11 == 0 && n >= 0) {
+        cout << "YES\n";
+    }else cout << "NO\n";
+}
