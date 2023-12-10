b6399c405d26b8a25329b66e6d96ebbe88a7480b
Harshil <savaliyaharshil3030@gmail.com>
2023-12-10 19:16:00 +0530

    Add solution for problem `A - Save the problem!` Link: https://codeforces.com/contest/865/problem/A Tags: constructive algorithms, *1400 Ref: https://codeforces.com/contest/865/submission/236661901

---
+++ codeforces/865/A.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long t = 0;
+#define ll    long long int
+#define gcd(a, b) __gcd((a), (b))
+#define lcm(a, b) (a * b) / gcd(a, b)
+const ll mod = 1000000007;
+const long double pi = 3.14159265358979323846264338327950288419716939937510582097494459230;
+#define int ll
+//-----------------------------------------------------------------------
+void solve();
+int bin_expo(int a, int b) {
+    int ans = 1;
+    while (b > 0) {
+        if (b % 2) {
+            ans = ans * a;
+        } b/=2;
+        a = a * a;
+    } return ans;
+}
+//-----------------------------------------------------------------------
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    // int tt = 1; cin >> tt; while (tt--)  
+    solve();
+    return 0;
+}
+//-----------------------------------------------------------------------
+void solve() {
+    int n; cin >> n;
+    if (n == 1) {
+        cout << 1 << " " << 1 << "\n";
+        cout << 1 << "\n";
+        return;
+    } 
+    int amount = n*2-2;
+    cout << amount << " " << 2 << "\n";
+    cout << 1 << " " << 2 << "\n";
+}
+
+
