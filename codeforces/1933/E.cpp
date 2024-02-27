71bab41ea44ec5803ff5d8eafa437aa98738d04a
Harshil <savaliyaharshil3030@gmail.com>
2024-02-27 22:00:00 +0530

    Add solution for problem `E - Turtle vs. Rabbit Race: Optimal Trainings` Link: https://codeforces.com/contest/1933/problem/E Tags: binary search, implementation, math, ternary search, *1500 Ref: https://codeforces.com/contest/1933/submission/248619352

--- codeforces/1933/E.cpp
+++ codeforces/1933/E.cpp
@@ -1,117 +1,251 @@
 #include<bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
-#define inf (1LL<<60)
-#define prDouble(x) cout << fixed << setprecision(10) << x
 using namespace __gnu_pbds;
 using namespace std;
-#define int long long
-#define lcd(a,b) __gcd(a,b)/(a*b)
+typedef long long ll;
+#define int ll
 typedef tree<int,null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> ordset;
-
-void pr(int x) {cerr << x;}
-void pr(long x) {cerr << x;}
-//void pr(long long x) {cerr << x;}
-void pr(unsigned x) {cerr << x;}
-void pr(unsigned long x) {cerr << x;}
-void pr(unsigned long long x) {cerr << x;}
-void pr(float x) {cerr << x;}
-void pr(double x) {cerr << x;}
-void pr(long double x) {cerr << x;}
-void pr(char x) {cerr << '\'' << x << '\'';}
-void pr(const char *x) {cerr << '\"' << x << '\"';}
-void pr(const string &x) {cerr << '\"' << x << '\"';}
-void pr(bool x) {cerr << (x ? "true" : "false");}
-
-template<typename T, typename V>
-void pr(const pair<T, V> &x) {cerr << '{'; pr(x.first); cerr << ','; pr(x.second); cerr << '}';}
-template<typename T>
-void pr(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), pr(i); cerr << "}";}
-void pr() {cerr << "]\n";}
-template <typename T, typename... V>
-void pr(T t, V... v) {pr(t); if (sizeof...(v)) cerr << ", "; pr(v...);}
-#ifndef ONLINE_JUDGE
-#define pr(x...) cerr << "[" << #x << "] = "; pr(x)
-#else
-#define pr(x...)
-#endif
-int mod = 1000000007;
-
-int bin_expo(int a, int b, int mod) {
-    int res = 1;
-    while(b > 0) {
-        if (b % 2) {
-            res = res * a % mod;
+int mod = 1e9 + 7;
+// numberTheory nt;
+class numberTheory {    
+    public:
+    int mod = 1e9 + 7;
+    std::vector<bool> sieve(int n) {
+        std::vector<bool> isPrime(n + 1, true);
+        isPrime[0] = isPrime[1] = false;
+    
+        for (int i = 2; i * i <= n; ++i) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
         }
-        b/=2;
-        a = a * a % mod;
+        return isPrime;
     }
-    return res;
-}
+    
+    std::vector<int> gen(vector<bool> prime) {
+        vector<int> v;
+        for(int i = 2; i < prime.size(); i++) {
+            if (prime[i] == true) {
+                v.push_back(i);
+            }
+        }
+        return v;
+    }
+    
+    std::vector<int> primeFactors(int n) {
+        std::vector<int> factors;
+    
+        while (n % 2 == 0) {
+            factors.push_back(2);
+            n /= 2;
+        }
+    
+        for (int i = 3; i <= std::sqrt(n); i += 2) {
+            while (n % i == 0) {
+                factors.push_back(i);
+                n /= i;
+            }
+        }
+    
+        if (n > 2) {
+            factors.push_back(n);
+        }
+    
+        return factors;
+    }
+    
+    long long bin_expo(long long base, long long exponent, long long mod = 1e9+7) {
+        long long result = 1;
+    
+        while (exponent > 0) {
+            if (exponent % 2 == 1) {
+                result = (result * base);
+            }
+    
+            base = (base * base);
+            exponent /= 2;
+        }
+    
+        return result;
+    }
+    
+    long long inverse(long long a, long long mod) {
+    return bin_expo(a, mod - 2, mod);    
+    }
 
-int inv(int a, int mod) {
-    return bin_expo(a, mod - 2, mod) % mod;
-}
+// Fast nPr (Permutation)
+    long long fastNPr(int n, int r) {
+        long long result = 1;
+        for (int i = n; i > n - r; --i) {
+            result = (result * i) % mod;
+        }
+        return result;
+    }
+    
+// Fast nCr (Combination)
+    long long NCr(int n, int r) {
+        if (r > n) return 0;
+        long long numerator = 1, denominator = 1;
+    
+        for (int i = 1; i <= r; ++i) {
+            numerator = (numerator * (n - i + 1)) % mod;
+            denominator = (denominator * i) % mod;
+        }
+    
+        return (numerator * inverse(denominator, mod)) % mod;
+    }
+    
+    // Euler's Totient Function (Phi)
+    int phi(int n) {
+        int result = n;
+        for (int i = 2; i * i <= n; ++i) {
+            if (n % i == 0) {
+                while (n % i == 0) {
+                    n /= i;
+                }
+                result -= result / i;
+            }
+        }
+        if (n > 1) {
+            result -= result / n;
+        }
+        return result;
+    }
+    
+    
+    // Check if a number is prime
+    bool isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; ++i) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    long long crt(const std::vector<long long>& a, const std::vector<long long>& m) {
+        // Assumes all moduli in m are pairwise coprime
+        int n = a.size();
+        long long M = 1, result = 0;
+    
+        for (int i = 0; i < n; ++i) {
+            M *= m[i];
+        }
+    
+        for (int i = 0; i < n; ++i) {
+            long long Mi = M / m[i];
+            result = (result + a[i] * Mi % M * inverse(Mi, m[i])) % M;
+        }
+    
+        return (result + M) % M;
+    }
+    
+    int mobiusFunction(int n) {
+        int count = 0;
+        for (int i = 2; i * i <= n; ++i) {
+            if (n % i == 0) {
+                ++count;
+    
+                if (count > 1 || (n / i) % i == 0) {
+                    return 0;  // n is not square-free
+                }
+    
+                n /= i;
+                --i;
+            }
+        }
+    
+        if (n > 1) {
+            ++count;
+        }
+    
+        return (count % 2 == 0) ? 1 : -1;
+    }
+    
+    int binomialCoefficientModP(int n, int r, int p) {
+        if (r == 0) return 1;
+    
+        int ni = n % p;
+        int ri = r % p;
+    
+        return (binomialCoefficientModP(n / p, r / p, p) *
+                binomialCoefficientModP(ni, ri, p)) % p;
+    }
+    
+    int lucasTheorem(int n, int r, int p) {
+        if (r == 0) return 1;
+    
+        int ni = n % p;
+        int ri = r % p;
+    
+        return (lucasTheorem(n / p, r / p, p) * binomialCoefficientModP(ni, ri, p)) % p;
+    }
 
+};
+numberTheory nt;
 void solve() {
     int n; cin >> n;
     vector<int> v(n);
-    for(auto &x : v) cin >> x;
-    int q; cin >> q;
-    vector<int> pref(n+1, 0);
+    for (auto &x : v )cin >> x;
+    vector<int> pref(n+1);
     pref[0] = 0;
-    for(int i = 1; i <= n; i++) pref[i] = pref[i-1] + v[i-1];
-    // pr(pref); cerr << "\n";
-    while(q--) {
-        int l, u; cin >> l >> u;
-        int lo = l, hi = n;
-        int ans = -1;
-        int curr_sm = 0;
-        while(lo <= hi) {
-            int mid = (lo + hi) >> 1;
-            int diff = pref[mid] - pref[l-1];
-            
-           
-
-            if (diff == u) {
-                ans = mid;
-                hi = mid - 1;
-            } if (diff > u) {
-                int c_ele = diff - (pref[mid - 1] - pref[l-1]);
-                int rest = u - (pref[mid - 1] - pref[l-1]);
-                pr(rest); cerr << "\n"; pr(l); cerr << "\n";
-                if (rest < 0) {
-                    hi = mid - 1;
-                    continue;
-                }
-
-                int n_n = c_ele - rest - 1;
-                int p_n = rest;
-                int t_n = n_n * (n_n + 1)/2;
-                int t_p = p_n * (p_n + 1)/2;
-              
-                if (t_n >= t_p) hi = mid - 1;
-                else {
-                    ans = mid;
-                    lo = mid + 1;
-                }
-            } else {
-                ans = mid;
-                lo = mid + 1;
-            }
+    for (int i = 1; i <= n; i++) {
+        pref[i] = pref[i-1] + v[i-1];
+    }
+    
+    auto search = [&](int need) -> int {
+      int lo = 1, hi = n;
+      int ans = 1;
+      while (lo <= hi) {
+          int mid = (lo + hi)/2;
+          // ans = mid;
+          if (pref[mid] > need) {
+              hi = mid - 1;
+          } else {
+              ans = mid;
+              lo = mid + 1;
+          }
+      }  
+      return ans;
+    };
+    
+    int q; cin >> q;
+    while (q--) {
+        int l,u; cin >> l >> u;
+        int ini = pref[l-1];
+        int need = ini + u;
+        int found = search(need);
+        int rest = u - (pref[found] - ini) ;
+        // cout << found << "\n";
+        // continue;
+        if (found == n) {
+            cout << max(found,l) << " ";
+            continue;
         }
-        if (ans == -1) {
-            cout << l << " ";
-        } else {
-            cout << ans << " ";
+        int howmuch = v[found] - rest;
+        // cout << rest << " " << howmuch << "\n";
+        // continue;
+        // cout << found << "\n";
+        // cout << howmuch << " " << rest << "\n";
+        // continue;
+        if (howmuch >= rest + 1) {
+            cout << max(found,l) << " ";
+        } else {    
+            cout << max(found + 1,l) << " ";
         }
+        
     } cout << "\n";
+    
 }
 
 signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    int tt = 1; cin >> tt; while (tt--)
-        solve();
+    int tt = 1; cin >> tt; while (tt--)  
+    solve();
     return 0;
 }
