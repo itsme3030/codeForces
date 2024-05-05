dba66eb4a7bb931752790c8b4e5da23103bb814e
Harshil <savaliyaharshil3030@gmail.com>
2024-05-05 20:06:00 +0530

    Add solution for problem `A - Enlarge GCD` Link: https://codeforces.com/contest/1034/problem/A Tags: number theory, *1800 Ref: https://codeforces.com/contest/1034/submission/259646203

--- codeforces/1034/A.cpp
+++ codeforces/1034/A.cpp
@@ -1,3 +1,4 @@
+//I am jack's wasted life
 #include<bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
@@ -8,13 +9,13 @@ typedef long long ll;
 typedef tree<int,null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> ordset;
 int mod = 1e9 + 7;
 // numberTheory nt;
-class numberTheory {
-public:
+class numberTheory {    
+    public:
     int mod = 1e9 + 7;
     std::vector<bool> sieve(int n) {
         std::vector<bool> isPrime(n + 1, true);
         isPrime[0] = isPrime[1] = false;
-
+    
         for (int i = 2; i * i <= n; ++i) {
             if (isPrime[i]) {
                 for (int j = i * i; j <= n; j += i) {
@@ -24,7 +25,7 @@ public:
         }
         return isPrime;
     }
-
+    
     std::vector<int> gen(vector<bool> prime) {
         vector<int> v;
         for(int i = 2; i < prime.size(); i++) {
@@ -34,46 +35,46 @@ public:
         }
         return v;
     }
-
+    
     std::vector<int> primeFactors(int n) {
         std::vector<int> factors;
-
+    
         while (n % 2 == 0) {
             factors.push_back(2);
             n /= 2;
         }
-
+    
         for (int i = 3; i <= std::sqrt(n); i += 2) {
             while (n % i == 0) {
                 factors.push_back(i);
                 n /= i;
             }
         }
-
+    
         if (n > 2) {
             factors.push_back(n);
         }
-
+    
         return factors;
     }
-
+    
     long long bin_expo(long long base, long long exponent, long long mod = 1e9+7) {
         long long result = 1;
-
+    
         while (exponent > 0) {
             if (exponent % 2 == 1) {
                 result = (result * base);
             }
-
+    
             base = (base * base);
             exponent /= 2;
         }
-
+    
         return result;
     }
-
+    
     long long inverse(long long a, long long mod) {
-        return bin_expo(a, mod - 2, mod);
+    return bin_expo(a, mod - 2, mod);    
     }
 
     long long fastNPr(int n, int r) {
@@ -83,19 +84,19 @@ public:
         }
         return result;
     }
-
+    
     long long NCr(int n, int r) {
         if (r > n) return 0;
         long long numerator = 1, denominator = 1;
-
+    
         for (int i = 1; i <= r; ++i) {
             numerator = (numerator * (n - i + 1)) % mod;
             denominator = (denominator * i) % mod;
         }
-
+    
         return (numerator * inverse(denominator, mod)) % mod;
     }
-
+    
     int phi(int n) {
         int result = n;
         for (int i = 2; i * i <= n; ++i) {
@@ -111,8 +112,8 @@ public:
         }
         return result;
     }
-
-
+    
+    
     bool isPrime(int n) {
         if (n < 2) return false;
         for (int i = 2; i * i <= n; ++i) {
@@ -122,68 +123,68 @@ public:
         }
         return true;
     }
-
+    
     long long crt(const std::vector<long long>& a, const std::vector<long long>& m) {
         int n = a.size();
         long long M = 1, result = 0;
-
+    
         for (int i = 0; i < n; ++i) {
             M *= m[i];
         }
-
+    
         for (int i = 0; i < n; ++i) {
             long long Mi = M / m[i];
             result = (result + a[i] * Mi % M * inverse(Mi, m[i])) % M;
         }
-
+    
         return (result + M) % M;
     }
-
+    
     int mobiusFunction(int n) {
         int count = 0;
         for (int i = 2; i * i <= n; ++i) {
             if (n % i == 0) {
                 ++count;
-
+    
                 if (count > 1 || (n / i) % i == 0) {
-                    return 0;
+                    return 0;  
                 }
-
+    
                 n /= i;
                 --i;
             }
         }
-
+    
         if (n > 1) {
             ++count;
         }
-
+    
         return (count % 2 == 0) ? 1 : -1;
     }
-
+    
     int binomialCoefficientModP(int n, int r, int p) {
         if (r == 0) return 1;
-
+    
         int ni = n % p;
         int ri = r % p;
-
+    
         return (binomialCoefficientModP(n / p, r / p, p) *
                 binomialCoefficientModP(ni, ri, p)) % p;
     }
-
+    
     int lucasTheorem(int n, int r, int p) {
         if (r == 0) return 1;
-
+    
         int ni = n % p;
         int ri = r % p;
-
+    
         return (lucasTheorem(n / p, r / p, p) * binomialCoefficientModP(ni, ri, p)) % p;
     }
 
 };
 numberTheory nt;
 class SGtree {
-public:
+    public:
     vector<int> seg;
     SGtree(int n) {
         seg.resize(4*n);
@@ -196,7 +197,7 @@ public:
         int mid = (high + low) >> 1;
         build (2*ind + 1, low, mid, arr);
         build (2*ind + 2, mid + 1, high, arr);
-        seg[ind] = min(seg[2*ind + 1], seg[2*ind+2]);
+        seg[ind] = min(seg[2*ind + 1], seg[2*ind+2]); 
     }
 
     int query(int ind, int low, int high, int l, int r) {
@@ -227,56 +228,48 @@ public:
 int mp[15000001];
 int cnt[15000001];
 void solve(){
-    for(int i = 2; i <= 15000000; i++) {
-        if (!mp[i]) {
-            for(int j = i; j <= 15000000; j += i) {
-                mp[j] = i;
-            }
-        }
-    }
-
-    int g = 0;
     int n;
     cin >> n;
     vector<int> v(n);
-    for(auto &x : v)  {
-        cin >> x;
-        g = __gcd(x, g);
-    }
-    for(auto &x : v) x/=g;
-    map<int, int> freq;
-    for(auto &x :v) {
-        int tmp = x;
-        set<int> fct;
-
-        while (tmp > 1) {
-            int p = mp[tmp];
-            while (tmp % p == 0) tmp/=p;
-            fct.insert(p);
+    for(auto &x : v) cin >> x;
+    for(int i = 2; i <= 15000000; i++) {
+        if (!mp[i]) {
+            for(int j = i; j <= 15000000; j += i) {
+                if(!mp[j]) mp[j] = i;
+            }
         }
+    }
+    int g = 0;
+    for (int i = 0; i < n; i++) {
+        g = __gcd(v[i], g);
+    }
 
-        if (tmp > 1) fct.insert(tmp);
-        for(auto &x : fct) freq[x]++;
+    for(int i = 0; i < n; i++) {
+        int ele = v[i];
+        ele/=g;
+        for (int j = ele; j > 1; ) {
+            int p = mp[j];
+            while (j % p == 0) j/=p;
+            cnt[p]++;
+        }
     }
-    if (freq.size() == 0) {
+
+    int mn = 1e9;
+    for(auto &x : cnt) {
+        mn = min(mn, n - x);
+    }
+    if (mn == n || mn == 1e9) {
         cout << -1 << "\n";
         return;
-    }
-    int ans = n+1;
-    for(auto &x : freq) {
-        ans = min(ans, n - x.second);
-    }
-    if (ans == n+1) {
-        cout << -1 << "\n";
-        return;
-    }
-    cout << ans << "\n";
+    } 
+    cout << mn << "\n";   
 }
 
 signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    // int tt = 1; cin >> tt; while (tt--)
+    // int tt = 1; cin >> tt; while (tt--)  
     solve();
     return 0;
 }
+ 
