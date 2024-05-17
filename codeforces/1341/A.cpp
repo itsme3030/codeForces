bfbec0454c1128bdd2455f7ded2f665c4a031dfe
Harshil <savaliyaharshil3030@gmail.com>
2024-05-17 15:24:00 +0530

    Add solution for problem `A - Nastya and Rice` Link: https://codeforces.com/contest/1341/problem/A Tags: math, *900 Ref: https://codeforces.com/contest/1341/submission/261309572

---
+++ codeforces/1341/A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define inf (1LL<<60)
+#define prDouble(x) cout << fixed << setprecision(10) << x
+using namespace __gnu_pbds;
+using namespace std;
+#define int long long
+typedef tree<int,null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> ordset;
+
+void solve() {
+   int n,a,b,c,d;
+   cin >> n >> a >> b >> c >> d;
+   int lowest = n*(a-b);
+   int highest = n*(a+b);
+   int lowestPacket = c-d;
+   int highestPacket = c+d;
+
+   if ((lowest < lowestPacket && highest < lowestPacket) || (lowest > highestPacket && highest > highestPacket)) {
+      cout << "No\n";
+      return;
+   } else {
+      cout << "Yes\n";
+      return;
+   }
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tt = 1; cin >> tt; while (tt--)  
+    solve();
+    return 0;
+}
