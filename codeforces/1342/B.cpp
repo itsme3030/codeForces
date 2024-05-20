27bbfb1f17db537c7f297ca7257b8860bae07dcc
Harshil <savaliyaharshil3030@gmail.com>
2024-05-20 11:48:00 +0530

    Add solution for problem `B - Binary Period` Link: https://codeforces.com/contest/1342/problem/B Tags: constructive algorithms, strings, *1100 Ref: https://codeforces.com/contest/1342/submission/261729182

--- codeforces/1342/B.cpp
+++ codeforces/1342/B.cpp
@@ -1,37 +1,35 @@
 #include<bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
+#define inf (1LL<<60)
+#define prDouble(x) cout << fixed << setprecision(10) << x
 using namespace __gnu_pbds;
 using namespace std;
-typedef long long ll;
-#define int ll
+#define int long long
 typedef tree<int,null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> ordset;
-void solve();
+
+void solve() {
+	string s;
+	cin >> s;
+	int n = s.length();  
+	bool isone = false, iszero = false;
+	for(int i = 0; i < n; i++) {
+		if (s[i] == '0') iszero = true;
+		if (s[i] == '1') isone = true;
+	}
+	if (iszero && isone) {
+		for(int i = 0; i < n; i++) {
+			cout << "0" << "1";
+		} cout << "\n";
+	} else {
+		cout << s << "\n";
+	}
+}
 
 signed main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    int tt = 1; cin >> tt; while (tt--)
+    int tt = 1; cin >> tt; while (tt--)  
     solve();
     return 0;
-}
-
-void solve() {
-    string s; cin >> s;
-    int n = s.length();
-    map<char, int> mp;
-    for (int i = 0; i < n; i++) {
-        mp[s[i]]++;
-    }
-    if (mp.size() == 1) {
-        for (auto &x : s) cout << x;
-        cout << "\n";
-        return;
-    }
-    for (int i = 0; i < 2*n; i++) {
-        if (i % 2) cout << 1;
-        else cout << 0;
-    }
-    cout << "\n";
-
 }
