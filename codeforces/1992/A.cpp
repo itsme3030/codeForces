464e29ca34f1d9a06fa31e8f3424823f1fe546e8
Harshil <savaliyaharshil3030@gmail.com>
2024-07-24 16:54:00 +0530

    Add solution for problem `A - Only Pluses` Link: https://codeforces.com/contest/1992/problem/A Tags: brute force, constructive algorithms, greedy, math, sortings, *800 Ref: https://codeforces.com/contest/1992/submission/272303694

---
+++ codeforces/1992/A.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define inf (1LL<<60)
+#define prDouble(x) cout << fixed << setprecision(10) << x
+using namespace __gnu_pbds;
+using namespace std;
+#define int long long
+#define lcd(a,b) __gcd(a,b)/(a*b)
+typedef tree<int,null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> ordset;
+
+void pr(int x) {cerr << x;}
+void pr(long x) {cerr << x;}
+//void pr(long long x) {cerr << x;}
+void pr(unsigned x) {cerr << x;}
+void pr(unsigned long x) {cerr << x;}
+void pr(unsigned long long x) {cerr << x;}
+void pr(float x) {cerr << x;}
+void pr(double x) {cerr << x;}
+void pr(long double x) {cerr << x;}
+void pr(char x) {cerr << '\'' << x << '\'';}
+void pr(const char *x) {cerr << '\"' << x << '\"';}
+void pr(const string &x) {cerr << '\"' << x << '\"';}
+void pr(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void pr(const pair<T, V> &x) {cerr << '{'; pr(x.first); cerr << ','; pr(x.second); cerr << '}';}
+template<typename T>
+void pr(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), pr(i); cerr << "}";}
+void pr() {cerr << "]\n";}
+template <typename T, typename... V>
+void pr(T t, V... v) {pr(t); if (sizeof...(v)) cerr << ", "; pr(v...);}
+#ifndef ONLINE_JUDGE
+#define pr(x...) cerr << "[" << #x << "] = "; pr(x)
+#else
+#define pr(x...)
+#endif
+int mod = 1000000007;
+
+int bin_expo(int a, int b, int mod) {
+    int res = 1;
+    while(b > 0) {
+        if (b % 2) {
+            res = res * a % mod;
+        }
+        b/=2;
+        a = a * a % mod;
+    }
+    return res;
+}
+
+int inv(int a, int mod) {
+    return bin_expo(a, mod - 2, mod) % mod;
+}
+
+void solve() {
+    int a,b,c; cin >> a >> b >> c;
+    int mx = 0;
+    for(int i = 0; i <= 5; i++) {
+        for(int j = 0; j <= 5; j++) {
+            for(int k = 0; k <= 5; k++) {
+                if (i + j + k > 5) continue;
+                int n_a = a + i, n_b = b + j, n_c = c + k;
+                mx = max(mx, n_a * n_b * n_c);
+            }
+        }
+    }
+    cout << mx << "\n";
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tt = 1; cin >> tt; while (tt--)
+        solve();
+    return 0;
+}
